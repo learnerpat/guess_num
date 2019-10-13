743d68805d7546806209651e96f75f4497b9ac42
learnerpat <466260pat@gmail.com>
2019-10-13 16:39:10 -0400

    udapte guess number

--- guess1.py
+++ guess1.py
@@ -1,12 +1,16 @@
 import random
 r = random.randint(1,100)
+count = 0
 while True:
-	num = input("请输入1到100的整数")
+	count += 1
+	num = input("请输入1到100的整数: ")
 	num = int(num)
 	if num == r:
 		print("你猜中了!")
+		print("这是你猜的第", count, "次.")
 		break
 	elif num > r:
 		print("答案大了")
 	else:
 		print("答案少了")
+	print("这是你猜的第", count, "次.")
